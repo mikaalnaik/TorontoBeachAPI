5602340a7495fd07d4dd1fc839246c5c3cc3da7d
Kieran Huggins <kieran@kieran.ca>
2020-06-21 22:02:17 -0400

    small refactor
    clean up args / validations
    comment out example usage

--- index.js
+++ index.js
@@ -2,39 +2,52 @@
 const fetch = require('node-fetch');
 const convert = require('xml-js');
 const get = require('lodash.get');
-const BeachConstants = require('./beachConstants')
+const BeachConstants = require('./beachConstants');
+
+DATE_REGEX = /^\d{4}-\d{2}-\d{2}$/;
 
 function getAllBeachesAllTime() {
-  return getFromApi('http://app.toronto.ca/tpha/ws/beaches/history/all.xml?v=1.0');
+  return getFromApi('beaches/history/all.xml?v=1.0');
 }
 
 function getAllBeachesLatest() {
-  return getFromApi('http://app.toronto.ca/tpha/ws/beaches.xml?v=1.0');
+  return getFromApi('beaches.xml?v=1.0');
 }
 
-function getAllBeachesForRange(start, endDate) {
-  if (!start || !end) {
-    throw 'Both a start date and end date are required. We also expect a date format of YYYY-MM-DD';
+function getAllBeachesForRange(startDate, endDate) {
+  if (!startDate || !endDate) {
+    throw 'Both a start date and end date are required.';
   }
-  return getFromApi(`http://app.toronto.ca/tpha/ws/beaches/history.xml?v=1.0&from=${start}&to=${endDate}`);
+
+  if (!DATE_REGEX.test(startDate) || !DATE_REGEX.test(endDate)) {
+    throw 'Dates must be formatted YYYY-MM-DD';
+  }
+
+  return getFromApi(`beaches/history.xml?v=1.0&from=${startDate}&to=${endDate}`);
 }
 
 function getSpecificBeachForRange(beachID, startDate, endDate) {
-  if (!startDate || !endDate || !beachID) {
-    throw 'Both a start date and end date are required. We also expect a date format of YYYY-MM-DD';
+  if (!beachID || !startDate || !endDate) {
+    throw 'The beach ID, start date, and end date are all required.';
   }
-  return getFromApi(`http://app.toronto.ca/tpha/ws/beach/${beachID}/history.xml?v=1.0&from=${startDate}&to=${endDate}`);
+
+  if (!DATE_REGEX.test(startDate) || !DATE_REGEX.test(endDate)) {
+    throw 'Dates must be formatted YYYY-MM-DD';
+  }
+
+  return getFromApi(`beach/${beachID}/history.xml?v=1.0&from=${startDate}&to=${endDate}`);
 }
 
 function getSpecificBeachAllTime(beachID) {
-  if (!startDate || !endDate || !beachID) {
-    throw 'Both a start date and end date are required. We also expect a date format of YYYY-MM-DD';
+  if (!beachID) {
+    throw 'The beach ID is required.';
   }
-  return getFromApi(`http://app.toronto.ca/tpha/ws/beach/${beachID}/history/all.xml?v=1.0`);
+
+  return getFromApi(`beach/${beachID}/history/all.xml?v=1.0`);
 }
 
-function getFromApi(url) {
-  return fetch(url)
+function getFromApi(path) {
+  return fetch(`http://app.toronto.ca/tpha/ws/${path}`)
     .then(res => {
       return res.text();
     })
@@ -47,6 +60,8 @@ function getFromApi(url) {
     });
 }
 
+/*
+// example usage, commented out so we don't fetch it when this module is included
 const test = getSpecificBeachForRange(1, '2019-02-01', '2019-09-31').then(res => {
   // console.log({ res });
 
@@ -54,42 +69,39 @@ const test = getSpecificBeachForRange(1, '2019-02-01', '2019-09-31').then(res =>
     // console.log({ item });
   });
 });
+*/
 
+const formatBeachData = (rawData) => {
+  const data = JSON.parse(
+    convert.xml2json(rawData, {
+      compact: false,
+      spaces: 4
+    })
+  );
 
-  const formatBeachData = (rawData) => {
-    const data = JSON.parse(
-      convert.xml2json(rawData, {
-        compact: false,
-        spaces: 4
-      })
-    );
+  const beaches = get(data, ['elements', 0, 'elements', 1, 'elements']);
 
-    const beaches = get(data, ['elements', 0, 'elements', 1, 'elements']);
-
-    return beaches
-      .map(beach => {
-        const beachData = get(beach, ['elements']);
-        const beachID = get(beach, ['attributes', 'beachId']);
-        return {
-          beachID: Number(beachID),
-          name: get(BeachConstants, [beachID, 'name']),
-          map: get(BeachConstants, [beachID, 'map']),
-          sampleDate: get(beachData, [0, 'elements', 0, 'text']),
-          publishDate: get(beachData, [1, 'elements', 0, 'text']),
-          eColiCount: Number(get(beachData, [2, 'elements', 0, 'text'])),
-          beachAdvisory: get(beachData, [3, 'elements', 0, 'text']),
-          beachState: get(beachData, [4, 'elements', 0, 'text'])
-        };
-      })
-      .filter(element => {
-        if (get(element, ['name'])) {
-          return element;
-        }
-      });
-  }
-
-
-
+  return beaches
+    .map(beach => {
+      const beachData = get(beach, ['elements']);
+      const beachID = get(beach, ['attributes', 'beachId']);
+      return {
+        beachID: Number.parseInt(beachID),
+        name: get(BeachConstants, [beachID, 'name']),
+        map: get(BeachConstants, [beachID, 'map']),
+        sampleDate: get(beachData, [0, 'elements', 0, 'text']),
+        publishDate: get(beachData, [1, 'elements', 0, 'text']),
+        eColiCount: Number.parseInt(get(beachData, [2, 'elements', 0, 'text'])),
+        beachAdvisory: get(beachData, [3, 'elements', 0, 'text']),
+        beachState: get(beachData, [4, 'elements', 0, 'text'])
+      };
+    })
+    .filter(element => {
+      if (get(element, ['name'])) {
+        return element;
+      }
+    });
+}
 
 module.exports = {
   getAllBeachesAllTime,
